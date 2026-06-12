fa962d7432f0974919c5549989edf9bc9084c5b1
Bhushan Padghan <106475476+Thunderz45@users.noreply.github.com>
2026-06-13 02:00:42 +0530

    feat: Remove text on Messi page

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ document.addEventListener('DOMContentLoaded', () => {
             
             // Update typing text and video source
             if (isMessi) {
-                startTyping("Messi is the Goat");
                 videoSource.src = 'messi.mp4';
             } else {
                 startTyping("Hi i am spidy");
